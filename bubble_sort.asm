3b7166789d267a7a5f06d25cdff85c0bcc4cd75b
vmm-mmv <mihai.vieru@isa.utm.md>
2024-04-12 19:24:40 +0300

    bubble sort implemented

--- bubble_sort.asm
+++ bubble_sort.asm
@@ -21,16 +21,31 @@ bubble_sort:
     ; r10 is size of array
     inner_loop:
         inc r9
+
         mov r11, r10-1
+        dec r11
         sub r11, r8
         cmp r9, r11
+        jge outer_loop
+
+        mov r11, [arr+((r9+1)*8)]
+        cmp [arr+r9], r11
+        jg swap
+
         jne inner_loop
 
-    mov r9, -1
-    inc r8
-    cmp r8, r10-1
-    jne bubble_sort
+    outer_loop:
+        mov r9, -1
+        inc r8
+        cmp r8, r10-1
+        jne bubble_sort
 
     ret
 
 swap:
+    mov r12, [arr+r9*8]
+    mov r11, [arr+((r9+1)*8)]
+
+    mov [arr+((r9+1)*8)], r12
+    mov [arr+r9*8], r11
+    jmp inner_loop
